166f1d24ebae2463c84ce927656e69f314416ab5
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2024-03-03 13:41:56 +0100

    removed module for app consistency

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,15 +1,15 @@
-import styles from "../styles/CSS/pages/nomatch.module.css";
+import "../styles/CSS/pages/nomatch.css";
 import { NavLink } from "react-router-dom";
 
 const NoMatch = () => {
     return (
         <main>
-            <div className={styles.errorContainer}>
-                <h2 className={styles.error}>404</h2>
-                <p className={styles.errorMessage}>
+            <div className="error-container">
+                <h2 className="error">404</h2>
+                <p className="error-message">
                     Oups! La page que vous demandez n'existe pas.
                 </p>
-                <NavLink to="/" className={styles.returnHome}>
+                <NavLink to="/" className="return-home">
                     Retourner sur la page d’accueil
                 </NavLink>
             </div>
